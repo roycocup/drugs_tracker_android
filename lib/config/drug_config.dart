eeb3578485cf93721d75ab8eb750da99e7158d21
Rodrigo Dias <rodrigo.dias@abovesurveying.com>
2025-11-12 18:21:01 +0000

    drugs config

--- lib/config/drug_config.dart
+++ lib/config/drug_config.dart
@@ -5,7 +5,9 @@ class DrugConfig {
 
   static const List<Drug> defaultDrugs = [
     Drug(name: 'Diazepam', tabletDoseMg: 10.0),
-    Drug(name: 'Doxylamide', tabletDoseMg: 25.0),
+    Drug(name: 'Doxylamine', tabletDoseMg: 25.0),
     Drug(name: 'Zolpidem', tabletDoseMg: 10.0),
+    Drug(name: 'Coffee', tabletDoseMg: 100.0),
+    Drug(name: 'Tea', tabletDoseMg: 100.0),
   ];
 }
